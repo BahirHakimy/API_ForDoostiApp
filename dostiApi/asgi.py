d6f0ee7a051b5e1e810c0f03ef9b0ab4101494c3
Bahir Hakimy <bahirhakimy2015@gmail.com>
2022-01-29 13:00:11 +0430

    setting error still exist

--- dostiApi/asgi.py
+++ dostiApi/asgi.py
@@ -1,12 +1,13 @@
+from cmath import log
 import os
-
 from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dostiApi.development")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dostiApi.development")
-
+print("jkadf")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
