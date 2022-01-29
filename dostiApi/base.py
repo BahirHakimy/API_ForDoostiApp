10121b461983266a7feca0d59a64a2ebba126188
Bahir Hakimy <bahirhakimy2015@gmail.com>
2022-01-29 18:53:41 +0430

    added trusted originsf

--- dostiApi/base.py
+++ dostiApi/base.py
@@ -134,6 +134,7 @@ MEDIA_URL = "/media/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+CSRF_TRUSTED_ORIGINS = ["https://dostiapi.herokuapp.com"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
