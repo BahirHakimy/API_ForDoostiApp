4132bac331fdd9e7fd1ef1a96ac8cf46261a6893
Bahir Hakimy <bahirhakimy2015@gmail.com>
2022-01-29 12:46:29 +0430

    setting error fixed

--- dostiApi/asgi.py
+++ dostiApi/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dostiApi.development")
 
 application = ProtocolTypeRouter(
     {
