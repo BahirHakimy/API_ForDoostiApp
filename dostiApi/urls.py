4608ee6e99000a01916162b3ec6581790c063279
Bahir Hakimy <bahirhakimy2015@gmail.com>
2022-01-31 13:06:35 +0430

    static files probleme

--- dostiApi/urls.py
+++ dostiApi/urls.py
@@ -13,3 +13,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
